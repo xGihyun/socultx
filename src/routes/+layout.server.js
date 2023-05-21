92fb57822b86fb03312597020a086d9909970043
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2023-05-21 14:51:44 +0800

    Saving progress, working on login

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,13 @@
+/** @type {import('./$types').LayoutServerLoad} */
+
+export async function load({ cookies}) {    
+    let userUIDCookie = cookies.get("userUID");
+    if (userUIDCookie) {
+        return {
+            isUserLoggedIn: true
+        }
+    }
+    return {
+        isUserLoggedIn: false
+    }
+}
