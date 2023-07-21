36ea9c0ade9f4a59a7a222e71f8c4ce86cfee02a
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2023-07-21 14:29:52 +0800

    Saving progress (3)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,13 @@
 
 	$: ({ supabase, session } = data);
 	$: searchResults = form?.results;
+	$: alreadySentRequests = $sentFriendRequests?.map((i) => i.receiver_id) ?? [];
+	// let alreadySentRequests: string[]
+	// $: alreadySentRequests = [];
+	// // $sentFriendRequests?.map((i) => i.receiver_id);
+	// sentFriendRequests.subscribe(
+	// 	(latest) => (alreadySentRequests = latest?.map((i) => i.receiver_id) ?? [])
+	// );
 
 	async function addFriend(uid: string, username: string) {
 		const { data, error } = await supabase
@@ -44,6 +51,11 @@
 		};
 
 		toastStore.trigger(sendFriendRequestToast);
+
+		$sentFriendRequests?.push(data[0]);
+		$sentFriendRequests = $sentFriendRequests; // This line is very important! must reassign writable!
+
+		alreadySentRequests = $sentFriendRequests?.map((i) => i.receiver_id) ?? [];
 	}
 </script>
 
@@ -64,21 +76,26 @@
 							</a>
 							<span class="font-gt-walsheim-pro-thin">{item.bio || ''}</span>
 						</div>
+
 						<!-- Add friend button -->
-						<div class="m-auto">
-							<button on:click={() => addFriend(item.id, item.username)}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="mr-2 h-6 w-6 fill-secondary-600"
-									height="1em"
-									viewBox="0 0 640 512"
-								>
-									<path
-										d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V136c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H552v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
-									/></svg
-								>
-							</button>
-						</div>
+						{#if alreadySentRequests?.includes(item.id)}
+							<div class="m-auto">Waiting...</div>
+						{:else}
+							<div class="m-auto">
+								<button on:click={() => addFriend(item.id, item.username)}>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="mr-2 h-6 w-6 fill-secondary-600"
+										height="1em"
+										viewBox="0 0 640 512"
+									>
+										<path
+											d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V136c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H552v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
+										/></svg
+									>
+								</button>
+							</div>
+						{/if}
 					</div>
 				</div>
 			{/each}
